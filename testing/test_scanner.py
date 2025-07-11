55b9cdefbbd74017889abf59419131b1f31a2079
HunewJeong <royaljelly95@gmail.com>
2025-07-11 21:38:57 +0900

    added timeout features

--- testing/test_scanner.py
+++ testing/test_scanner.py
@@ -1,37 +1,46 @@
 import asyncio # Event loop, Coroutines, Non blocking networking
 import argparse # Simplifies command-line argument parsing for user input
+from datetime import datetime
 
 """ 
 We choose asyncio because: port scanning involves many concurrent network I/O
 argparse lets us expose --targets --ports flags easily
+datetime: Measure per-probe durations
 """
 
-async def scan_port(ip, port):
+async def scan_port(ip, port, timeout=1.0):
     """
     attempts an asynchronous TCP connection to (ip, port)
     Returns (ip, port, Bool)
     """
+    start = datetime.now()
+    is_open = False
     try:
         # Open a connection; under the hood this sends a SYN and awaits response
-        reader, writer = await asyncio.open_connection(ip, port)
+        # Aborts connection if no response within 'timeout' seconds
+        conn_coro = asyncio.open_connection(ip, port)
+        reader, writer = await asyncio.wait_for(conn_coro, timeout)
+        is_open = True
         writer.close() # close cleanly if successful
         await writer.wait_closed()
-
-        return ip, port, True
+    except asyncio.TimeoutError:
+        print(f"{ip}:{port} TIMEOUT after {timeout}s")
     except Exception as e:
         print(e)
-        return ip, port, False
+    
+    duration = (datetime.now() - start).total_seconds()
+    print(f"{ip}:{port} {'OPEN' if is_open else 'CLOSED'}")
+    return ip, port, is_open, duration
 
-async def main(targets, ports):
+async def main(targets, ports, timeout):
     # Build a list of coroutines; one per target-port pair
-    tasks = [scan_port(ip, port) for ip in targets for port in ports]
+    tasks = [scan_port(ip, port, timeout)
+             for ip in targets for port in ports
+            ]
     
     # Run them concurrently and wait for all to finish
-    results = await asyncio.gather(*tasks)
+    await asyncio.gather(*tasks)
 
-    for ip, port, is_open in results:
-        status = 'OPEN' if is_open else 'CLOSED'
-        print(f"{ip}:{port} {status}")
 
 
 if __name__ == "__main__":
@@ -40,7 +49,9 @@ if __name__ == "__main__":
                         help = 'List of IPs to scan')
     parser.add_argument('--ports', nargs='+', type = int, required= True,
                         help = 'List of ports to scan')
+    parser.add_argument('--timeout', type=float, default = 1.0,
+                        help = 'Give up on a probe after x seconds')
     args = parser.parse_args()
 
     # Start the asyncio event loop
-    asyncio.run(main(args.targets, args.ports))
+    asyncio.run(main(args.targets, args.ports, args.timeout))
